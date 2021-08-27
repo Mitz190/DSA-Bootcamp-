6365cae434dde2f036c6d095f5d83e846fdc75b7
Mithali Redkar <86156837+Mitz190@users.noreply.github.com>
2021-08-27 16:34:26 +0530

    Factorial.java created

---
+++ Factorial.java
@@ -0,0 +1,34 @@
+//Q: Write a program to print factorial of a number, also take input.
+import java.util.Scanner;
+public class Factorial
+{
+  public static void main(String args[])
+  {
+    int n, c, f = 1;
+
+    System.out.println("Enter an integer to calculate its factorial");
+    Scanner in = new Scanner(System.in);
+
+    n = in.nextInt();
+
+    if (n < 0)
+      System.out.println("Number should be non-negative.");
+    else
+    {
+      for (c = 1; c <= n; c++)
+        f = f*c;
+
+      System.out.println("Factorial of "+n+" is = "+f);
+    }
+  }
+}
+
+/*
+output:
+if user enters -3
+output :Number should be non-negative.
+
+
+If user enter 4
+Factorial of 4 is 24
+*/
