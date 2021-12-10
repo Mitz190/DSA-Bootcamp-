9b3380d39561f99188a22e986701b20ed56dd0ca
Mithali Redkar <86156837+Mitz190@users.noreply.github.com>
2021-12-10 10:10:15 +0530

    Updated pattern .java

--- Pattern.java
+++ Pattern.java
@@ -82,4 +82,24 @@ public class Numbers{
     }
 }
 
+/*
+    *****
+    ****
+    ***
+    **
+    *
+*/
+public class pattern{
+    public static void main(String args[]){
+        for (int i=1; i<=5; i++)
+        {
+            for (int j=1; j<=5-i+1; j++)
+            {    
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
+
 
